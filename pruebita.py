15912fb1ae7f4dfba6f5a7fd03311dfd385783f2
Emiliano Lopez <emiliano.lopez@gmail.com>
2022-11-01 14:16:43 +0100

    agrego cos, renombro funcion matemática

--- pruebita.py
+++ pruebita.py
@@ -2,12 +2,16 @@ import numpy as np
 import plotly.graph_objects as go
 
 x = np.arange(0,1,0.01)
-y = np.sin(2*np.pi*x)
+f1 = np.sin(2*np.pi*x)
+f2 = np.cos(2*np.pi*x)
 
-print("Hola mundo!")
-print(x)
-print(y)
+
+#print("Hola mundo!")
+#print(x)
+#print(y)
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=x,y=y, mode="markers"))
+fig.add_trace(go.Scatter(x=x,y=f1, mode="markers"))
+fig.add_trace(go.Scatter(x=x,y=f2, mode="markers"))
+
 fig.show()
